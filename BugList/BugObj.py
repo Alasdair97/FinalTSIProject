0f4d8aff126c7c009a19704ee2a9ab99f395e8d6
Alasdair97 <alasdairw@me.com>
2022-06-06 16:12:35 +0100

    Better def'd Bug class & made description optional

--- BugList/BugObj.py
+++ BugList/BugObj.py
@@ -6,10 +6,14 @@ class Bug(ABC):
     Bug_ID_Counter = 0
 
     # Constructors
-    def __init__(self, servarity, type, description):
+    def __init__(self, servarity, type, error_code):
         self.Value = "Bug"
-        self.Servarity = servarity
-        self.Type = type
-        self.Description = description
+        self.Servarity = str(servarity)
+        self.Type = str(type)
+        self.Code = str(error_code)
         self.id = Bug.Bug_ID_Counter
         Bug.Bug_ID_Counter += 1
+
+    # Methods
+    def type(self, description):
+        self.Description = str(description)
