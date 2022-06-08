4a4cc82aedc816c7f5ab5610a940e9a242aa7e75
Alasdair97 <alasdairw@me.com>
2022-06-08 11:59:00 +0100

    Created propper POST methods for api

--- BugList/BugListAPI.py
+++ BugList/BugListAPI.py
@@ -4,16 +4,46 @@ from flask import request, jsonify, Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+# Test Bug
+bugs = [
+    {
+        'id': 1,
+        'name': 'The First Bug',
+        'description': 'In the begining there was a bug',
+        'department': 'general',
+        'priority': 'High'
+    },
+    {
+        'id': 2,
+        'name': 'The Second Bug',
+        'description': 'In the begining there was lots of bugs',
+        'department': 'general',
+        'priority': 'Medium'
+    },
+]
 
 # Basic messeage framework
 
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    # send bug to correct queue/bus/howerever I get this to work
+    # Send bad bugs to log queue
+    # Check if has a priority
 
-@app.route('/', methods=[
-    'GET'])  # which HTTP method we are using (GET) what route (extra bit of the URL) this method will be activated on.
-def home():
-    return (
-        "<h1>Automated bug reporting</h1><p>Internal System</p>"  # what the api returns
-    )
+    if request.method == 'GET':
+        for bug in bugs:
+            if bug['priority'] not in {"High", "Medium", "Low"}:
+                return "<p>Bad priority detected send to logs </p>"
+        return jsonify({'bugs': bugs})
+    elif request.method == 'POST':
+        if not 'priority' in request.json or request.json['priority'] not in {"High", "Medium", "Low"}:
+            # send to log queue
+            return 'Bad priority detected send to the logs', 400 #request.json #send to the logs
+        if request.json['priority'] in {"High","Critical"}:
+            return 'High priority send to Slack queue', 200 #request.json 
+        if request.json['priority'] in {"Medium","Low"}:
+            return 'Not High priority send to Jira queue', 200 #request.json 
+    
 
 
 if __name__ == '__main__':
